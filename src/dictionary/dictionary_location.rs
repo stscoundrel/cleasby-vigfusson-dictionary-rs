60b7cc482e0b4afb4db549132af1f94907e1804b
stscoundrel <sampo@pixels.fi>
2021-09-26 16:21:05 +0300

    Dictionary paths: add module/enum for deciding location

---
+++ src/dictionary/dictionary_location.rs
@@ -0,0 +1,30 @@
+const DICTIONARY_PATH: &str = "src/dictionary/dataset/cleasby-vigfusson.json";
+const NO_MARKUP_DICTIONARY_PATH: &str = "src/dictionary/dataset/cleasby-vigfusson-no-markup.json";
+
+pub enum DictionaryLocation {
+    MarkupDictionary,
+    NoMarkupDictionary,
+}
+
+impl DictionaryLocation { 
+    pub fn get_path(&self) -> String { 
+        match *self { 
+            DictionaryLocation::MarkupDictionary => String::from(DICTIONARY_PATH), 
+            DictionaryLocation::NoMarkupDictionary => String::from(NO_MARKUP_DICTIONARY_PATH)
+        }
+    } 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gets_dictionary_paths() {
+        let result1 = DictionaryLocation::MarkupDictionary.get_path();
+        let result2 = DictionaryLocation::NoMarkupDictionary.get_path();
+
+        assert_eq!(result1, DICTIONARY_PATH);
+        assert_eq!(result2, NO_MARKUP_DICTIONARY_PATH);
+    }
+}
